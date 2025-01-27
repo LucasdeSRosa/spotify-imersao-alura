0d5de41680f2ec5017b4c2a58a1681a9947df109
Lucas Rosa <rosa.lucas.contato@gmail.com>
2025-01-27 20:35:23 -0300

    alteracoes visuais de margin e padding e alteracoes textuais

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ const currentHour = new Date().getHours();
 // Forma mais simples
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
-    ? "Bom dia"
+    ? "Bom dia!"
     : currentHour >= 12 && currentHour < 18
-    ? "Boa tarde"
-    : "Boa noite";
+    ? "Boa tarde!"
+    : "Boa noite!";
 
 greetingElement.textContent = greetingMessage;
 
